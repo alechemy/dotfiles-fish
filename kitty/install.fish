d803cdecfdcf5af9723b5f47a84dce09ae388909
Alec Custer <alcuster@icloud.com>
2022-02-04 23:48:08 -0500

    add catppuccin theme to fish and kitty

--- kitty/install.fish
+++ kitty/install.fish
@@ -2,3 +2,4 @@
 mkdir -p $HOME/.config/kitty/
 curl -sL https://raw.githubusercontent.com/trygveaa/kitty-kitten-search/master/search.py -o $HOME/.config/kitty/search.py
 curl -sL https://raw.githubusercontent.com/trygveaa/kitty-kitten-search/master/scroll_mark.py -o $HOME/.config/kitty/scroll_mark.py
+curl -sL https://raw.githubusercontent.com/catppuccin/kitty/main/catppuccin.conf -o $HOME/.config/kitty/catppuccin.conf
