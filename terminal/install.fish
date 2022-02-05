65a9d1c616e3a3b73787afd92ec6d09606235497
Alec Custer <alcuster@icloud.com>
2022-02-05 09:43:23 -0500

    fix dupe terminal profile bug

--- terminal/install.fish
+++ terminal/install.fish
@@ -1,4 +1,11 @@
 #!/usr/bin/env fish
-open -a Terminal.app "$DOTFILES/terminal/Peppermint.terminal"
-defaults write com.apple.terminal "Default Window Settings" -string "Peppermint"
-defaults write com.apple.terminal "Startup Window Settings" -string "Peppermint"
+
+# If Peppermint theme is already set, don't bother setting it again, because
+# doing so will create duplicate entries in Terminal.app's preferences.
+set current (defaults read com.apple.terminal "Startup Window Settings")
+
+if test $current != "Peppermint"
+  open -a Terminal.app "$DOTFILES/terminal/Peppermint.terminal"
+  defaults write com.apple.terminal "Default Window Settings" -string "Peppermint"
+  defaults write com.apple.terminal "Startup Window Settings" -string "Peppermint"
+end
