490587b99ca81113ad2e4461428e56417fe4b746
Alec Custer <alcuster@icloud.com>
2022-02-09 12:44:05 -0500

    change macOS joinMode default

--- macos/set-defaults.fish
+++ macos/set-defaults.fish
@@ -23,3 +23,5 @@ defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile
 # Speed up the dock hide/show animation
 defaults write com.apple.dock autohide-delay -float 0.1
 defaults write com.apple.dock autohide-time-modifier -float 0.5
+# Prefer strongest signal
+sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport prefs joinMode=Strongest
