3f6807cd8878b6eb1d4fe89893a5c3c7b6eb3aec
Alec Custer <alcuster@icloud.com>
2022-02-10 17:12:51 -0500

    tell homebrew to automatically run brew cleanup

--- homebrew/install.fish
+++ homebrew/install.fish
@@ -3,4 +3,4 @@
 # TO-DO: configure this to run only if `brew autoupdate status` reports
 # that the autoupdate service is not currently running.
 
-brew autoupdate start --upgrade
+brew autoupdate start --upgrade --cleanup
