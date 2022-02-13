8a9fdffce629c26b1c80008015cc3c36965509a4
Alec Custer <alcuster@icloud.com>
2022-02-13 10:31:02 -0500

    add mkdir to espanso install.fish

--- espanso/install.fish
+++ espanso/install.fish
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 set ESPANSO_HOME "$HOME/Library/Application Support/espanso"
 
+mkdir -p "$ESPANSO_HOME/config"
+mkdir -p "$ESPANSO_HOME/match"
+
 ln -sf $DOTFILES/espanso/config/* $ESPANSO_HOME/config/
 ln -sf $DOTFILES/espanso/match/* $ESPANSO_HOME/match/
