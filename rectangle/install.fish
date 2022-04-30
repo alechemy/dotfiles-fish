d3193e48b2fac4750e7e07d775befad308755e5b
Alec Custer <alcuster@icloud.com>
2022-04-29 17:32:32 -0700

    add simple rectangle install script

---
+++ rectangle/install.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+# Increase stash size to 10px, from default of 1px
+defaults write com.knollsoft.Hookshot stashCursorBoxWidth -float 10
+defaults write com.knollsoft.Hookshot stashVisibleWidth -float 10
