ee07e2608f1bb33558f76d9469099d284cd416f7
Alec Custer <alcuster@icloud.com>
2023-09-22 07:45:34 -0700

    daily time machine backup

---
+++ macos/time-machine/install.fish
@@ -0,0 +1,17 @@
+#!/usr/bin/env fish
+
+set LAUNCHAGENTS_HOME "$HOME/Library/LaunchAgents/"
+
+ln -sf "$DOTFILES/macos/time-machine/com.alec.DailyTimeMachineBackup.plist" "$LAUNCHAGENTS_HOME/com.alec.DailyTimeMachineBackup.plist"
+
+# Wake daily at 1am
+sudo pmset repeat wake MTWRFSU 00:01:00
+
+# Install LaunchAgent that will back up daily at 1:05am
+set userId (launchctl manageruid)
+
+if launchctl list | grep -q com.alec.DailyTimeMachineBackup
+    launchctl bootout gui/501 $LAUNCHAGENTS_HOME/com.alec.DailyTimeMachineBackup.plist
+end
+
+launchctl bootstrap gui/$userId/ $LAUNCHAGENTS_HOME/com.alec.DailyTimeMachineBackup.plist
