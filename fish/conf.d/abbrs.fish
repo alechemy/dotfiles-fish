d7f796e976566f2275648ed9b86a7b5a51fbe09a
Alec Custer <alcuster@icloud.com>
2023-10-26 09:14:39 -0700

    add killport abbr

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -1,7 +1,15 @@
+#!/usr/bin/env fish
+
 # Add new abbrs to this file.
 # Must run `./00-dotfiles/install.fish` to install new abbrs.
 abbr -a -- beetlog 'tail -f ~/.config/beets/beets.log'
 abbr -a -- glog 'git log -n10 --oneline'
 abbr -a -- rm 'rm -I'
 abbr -a -- unpop 'git reset --merge'
-abbr -a -- verso '/Users/alec/Work/verso && nvm use'
+
+# Kill a process running on a given port
+# Usage: > kill_port 8081
+function kill_port
+    bash -c "kill -9 $(lsof -ti tcp:$argv)"
+end
+abbr -a killport --function kill_port
